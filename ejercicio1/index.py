a5548d81470975174ac97d44d246e7ed20b46adc
Diony Reyes <diony1991@gmail.com>
2022-08-26 14:27:04 -0400

    init commit

---
+++ ejercicio1/index.py
@@ -0,0 +1,11 @@
+from operations import BasicOperations
+
+add = BasicOperations.addition(4, 3)
+sub = BasicOperations.subtraction(4, 3)
+mul = BasicOperations.multiplication(4, 3)
+div = BasicOperations.division(4, 3)
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
---
+++ ejercicio1/operations/BasicOperations.py
@@ -0,0 +1,14 @@
+def addition(value1, value2):
+    return value1 + value2
+
+
+def subtraction(value1, value2):
+    return value1 - value2
+
+
+def multiplication(value1, value2):
+    return value1 * value2
+
+
+def division(value1, value2):
+    return value1 / value2
