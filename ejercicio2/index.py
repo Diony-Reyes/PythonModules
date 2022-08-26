e0617e630ff604dc5ff02041b6e2c5ec2bfa9298
Diony Reyes <diony1991@gmail.com>
2022-08-26 15:03:55 -0400

    some notes

--- ejercicio2/index.py
+++ ejercicio2/index.py
@@ -1,10 +1,13 @@
+# import time (this import the complete module)
+
 from time import localtime
 
 hour24 = localtime().tm_hour
+minutes = localtime().tm_min
 
 if hour24 >= 19:
     print("Es hora de ir a casa")
 
 if hour24 < 19:
-    print("Tiempo restante para ir a casa:", 19 - hour24)
+    print("Tiempo restante para ir a casa: {} horas y {} minutos".format(18 - hour24, 59 - minutes))
 
